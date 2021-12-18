5ce21839eb477f1fe398bf22b57bc7fea9e8f17c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-12-18 22:55:09 +0530

    sm8350-common: Fix USB OTG detection
     * in lahaina (msm-5.4) pmic driver now lies in adsp [1], so
       adsp firmware must to be loaded along with qti battery/
       charger kernel module, in order to get battery and usb
       power_supply up & running
    
     * with this, we dont need to set usb mode to peripheral
       explicitly either
    
    [1]: https://source.codeaurora.org/quic/la/device/qcom/common/commit/rootdir/etc/init.recovery.qcom.rc?h=LA.UM.9.14.r1-18600-LAHAINA.0&id=4e2b0f663a8f4b73000c81999055363411aaf12c
    Change-Id: Ic136e39b7555c95a0d9f91e22e370c47f7fe7e1b

--- common.mk
+++ common.mk
@@ -558,7 +558,7 @@ PLATFORM_VERSION := 127
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 
-TW_LOAD_VENDOR_MODULES := "xiaomi_touch.ko fts_touch_spi.ko fts_touch_spi_k2.ko focaltech_touch.ko"
+TW_LOAD_VENDOR_MODULES := "xiaomi_touch.ko fts_touch_spi.ko fts_touch_spi_k2.ko focaltech_touch.ko adsp_loader_dlkm.ko qti_battery_charger.ko"
 
 TARGET_RECOVERY_DEVICE_MODULES += \
     libandroidicu \
