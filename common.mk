73482f33e99156ac8e67ff30682117e35bfe4075
Nicholas Magill <nmagill1979@gmail.com>
2022-01-21 18:18:39 +0000

    sm8350-common: Build bootctrl hal
    Change-Id: I9cacd7411302b39084b0c8d83c78563d5eb76ba9

--- common.mk
+++ common.mk
@@ -134,7 +134,9 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
     android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.1-service \
+    bootctrl.xiaomi_sm8350 \
+    bootctrl.xiaomi_sm8350.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
